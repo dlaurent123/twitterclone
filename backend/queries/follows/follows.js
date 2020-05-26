b5f0ee9862b9960abef840bac0c434521a7af873
dannyl <danilovlaurent@pursuit.org>
2020-05-26 11:31:41 -0400

    create query and route to delete a follow

--- backend/queries/follows/follows.js
+++ backend/queries/follows/follows.js
@@ -10,10 +10,20 @@ const createFollow = async (req, res) => {
     res.status(200).json({ status: 200, message: "success" });
   } catch (error) {
     res.status(400).json({ status: 400, message: "bad request" });
-    console.log(error);
   }
 };
 
-const unFollow = async (req, res) => {};
+const unFollow = async (req, res) => {
+  const { followerId, userFollowedId } = req.body;
+  try {
+    await db.none(
+      "DELETE FROM follows WHERE follower_id = $1 AND user_followed_id = $2",
+      [followerId, userFollowedId]
+    );
+    res.status(200).json({ status: 200, message: "success" });
+  } catch (error) {
+    res.status(400).json({ status: 400, message: "bad request" });
+  }
+};
 
 module.exports = { createFollow, unFollow };
