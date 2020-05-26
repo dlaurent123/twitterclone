2d8c2e29cabf4f0bc5cc52099305e78c972cbc1c
dannyl <danilovlaurent@pursuit.org>
2020-05-26 13:21:10 -0400

    create route and query to unlike a post

--- backend/queries/likes/likes.js
+++ backend/queries/likes/likes.js
@@ -13,6 +13,17 @@ const createLike = async (req, res) => {
   }
 };
 
-const deleteLike = async (req, res) => {};
+const deleteLike = async (req, res) => {
+  const { likerId, postId } = req.body;
+  try {
+    await db.none(
+      "DELETE FROM likes WHERE liker_id = $1 AND liked_post_id = $2",
+      [likerId, postId]
+    );
+    res.status(200).json({ status: 200, message: "post unliked" });
+  } catch (error) {
+    res.status(400).json({ status: 400, message: "bad request" });
+  }
+};
 
 module.exports = { createLike, deleteLike };
