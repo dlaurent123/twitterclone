6ac51127a4baf407f7c2ff97ae45f8edec1c6b50
dannyl <danilovlaurent@pursuit.org>
2020-07-07 00:20:32 -0400

    add extra styling to tweet modal

--- frontend/src/features/home/tweetModal/tweetBody/TweetBody.js
+++ frontend/src/features/home/tweetModal/tweetBody/TweetBody.js
@@ -1,6 +1,7 @@
 import React from "react";
 import avi from "../../../../images/user.png";
 import "../Css/tweetmodal.css";
+import Button from "../../../login/button/Button";
 
 const TweetBody = () => {
   return (
@@ -10,8 +11,16 @@ const TweetBody = () => {
           <div className="tweetAvi">
             <img alt="" src={avi} />
           </div>
-          <div className="tweetText">
-            <span>Whats Happening?</span>
+          <div className="tweetContent">
+            <div className="tweetText">
+              <textarea maxLength="250" placeholder="What's Happening?" />
+            </div>
+            <div className="tweetFooter">
+              <div className="tweetAddOns"></div>
+              <div className="tweetModalButtonContainer">
+                <Button bDivId={"tweetModalButton"} text={"Tweet"} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
