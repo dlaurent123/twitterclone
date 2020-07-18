5d4e94de77369ab4771c3ba4d0780858def9e438
dannyl <danilovlaurent@pursuit.org>
2020-07-17 21:26:54 -0400

    complete logout modal

--- frontend/src/features/home/navBar/navLogout/NavLogOut.js
+++ frontend/src/features/home/navBar/navLogout/NavLogOut.js
@@ -1,49 +1,17 @@
 import React, { useState } from "react";
 import "./css/navLogOut.css";
-import { useSelector } from "react-redux";
-import { userState } from "../../../loggedInUserInfo/loggedInUserInfoSlice";
-import avi from "../../../../images/user.png";
-import down from "../../../../images/arrow.png";
 import LogOutModal from "./logOutModal/LogOutModal";
+import LogOutCont from "./logoutCont/LogOutCont";
 
 const NavLogOut = () => {
-  const state = useSelector(userState);
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <LogOutModal isOpen={isOpen} />
+      <LogOutModal setIsOpen={setIsOpen} isOpen={isOpen} />
       <div onClick={() => setIsOpen(true)} className="navLogOut">
         <div className="navLogOutTop">
-          <div className="navLogOutCont">
-            <div className="aviImg">
-              <img
-                style={{ height: "37px" }}
-                alt=""
-                src={state.user.avatar ? state.user.avatar : avi}
-              />
-            </div>
-            <div className="uInfo">
-              <div>
-                <span style={{ fontSize: "15px", fontWeight: "bolder" }}>
-                  {state.user.name}
-                </span>
-                <div>
-                  <span
-                    style={{
-                      fontSize: "15px",
-                      color: "rgb(136, 153, 166)",
-                    }}
-                  >
-                    @{state.user.user_name}
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div>
-              <img style={{ height: "15px" }} alt="" src={down} />
-            </div>
-          </div>
+          <LogOutCont />
         </div>
       </div>
     </>
--- frontend/src/features/home/navBar/navLogout/logOutModal/LogOutModal.js
+++ frontend/src/features/home/navBar/navLogout/logOutModal/LogOutModal.js
@@ -2,9 +2,15 @@ import React from "react";
 import Modal from "react-modal";
 import { signOutStyles } from "../../../../modal/helpers/helpers";
 import { logOut } from "../../../../../util/firebaseFunctions";
-import { clearUser } from "../../../../loggedInUserInfo/loggedInUserInfoSlice";
-import { useDispatch } from "react-redux";
-const LogOutModal = ({ isOpen }) => {
+import {
+  clearUser,
+  userState,
+} from "../../../../loggedInUserInfo/loggedInUserInfoSlice";
+import { useDispatch, useSelector } from "react-redux";
+import avi from "../../../../../images/user.png";
+import check from "../../../../../images/tick.png";
+const LogOutModal = ({ isOpen, setIsOpen }) => {
+  const state = useSelector(userState);
   const dispatch = useDispatch();
   const lo = () => {
     logOut();
@@ -15,11 +21,59 @@ const LogOutModal = ({ isOpen }) => {
       ariaHideApp={false}
       ariaModal={true}
       isOpen={isOpen}
-      // onAfterOpen={afterOpenModal}
+      shouldCloseOnOverlayClick={true}
+      onRequestClose={() => setIsOpen(false)}
       style={signOutStyles}
       contentLabel="Example Modal"
     >
-      <button onClick={lo}>Logout</button>
+      <div id="navLogOutCont" className="navLogOutCont">
+        <div className="aviImg">
+          <img
+            style={{ height: "37px" }}
+            alt=""
+            src={state.user.avatar ? state.user.avatar : avi}
+          />
+        </div>
+        <div className="uInfo">
+          <div>
+            <span style={{ fontSize: "15px", fontWeight: "bolder" }}>
+              {state.user.name}
+            </span>
+            <div>
+              <span
+                style={{
+                  fontSize: "15px",
+                  color: "rgb(136, 153, 166)",
+                }}
+              >
+                @{state.user.user_name}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div
+          style={{ display: " flex", alignContent: "center", padding: "5px" }}
+        >
+          <img style={{ height: "17px" }} alt="" src={check} />
+        </div>
+      </div>
+
+      <div>
+        <div
+          className="logOutSection"
+          onClick={lo}
+          style={{
+            padding: " 15px",
+            borderBottomStyle: "solid",
+            borderBottomColor: "#38444d",
+            borderBottomWidth: "thin",
+          }}
+        >
+          <span style={{ fontSize: "15px", fontWeight: "500" }}>
+            Log out @{state.user.user_name}
+          </span>
+        </div>
+      </div>
     </Modal>
   );
 };
