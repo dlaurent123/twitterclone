68974781ea9dbb8f19a99a59b68101c94edea3fe
dannyl <danilovlaurent@pursuit.org>
2020-08-03 22:27:47 -0400

    create posts component

--- frontend/src/features/home/Home.js
+++ frontend/src/features/home/Home.js
@@ -1,18 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { useSelector } from "react-redux";
 import gif from "../../images/blue.png";
 import { userState } from "../loggedInUserInfo/loggedInUserInfoSlice";
 import "./css/home.css";
+import axios from "axios";
+import { apiUrl } from "../../util/apiUrl";
+import { AuthContext } from "../../providers/AuthContext";
+import Posts from "../posts/Posts";
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(true);
   const state = useSelector(userState);
+  const API = apiUrl();
+  const { token } = useContext(AuthContext);
+  const [posts, setPosts] = useState([
+    {
+      avatar: null,
+      name: "deploy jones",
+      post_body: "second test tweet",
+      post_id: 17,
+      post_image: null,
+      post_time: "2020-07-18T15:09:35.027Z",
+      user_id: "y15mmoTQeEWk5bc3M7mq0ZFTAqI3",
+      user_name: "jones",
+    },
+  ]);
 
   useEffect(() => {
+    const getAllPosts = async () => {
+      try {
+        let res = await axios({
+          method: "GET",
+          url: `${API}/api/users/posts`,
+          headers: {
+            AuthToken: token,
+          },
+        });
+        // debugger;
+        // setPosts(res.data.posts);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    // getAllPosts();
     if (state.user) {
       setIsLoading(false);
     }
-  }, [state.user]);
+  }, [state.user, API, token]);
 
   if (isLoading) {
     return (
@@ -33,6 +67,7 @@ const Home = () => {
               </h2>
             </div>
           </div>
+          <Posts posts={posts} />
         </div>
       </div>
       <div className="searchSide">Search</div>
