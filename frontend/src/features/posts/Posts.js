d014f773f5cc1d017795e3a8da542475aa73d141
dannyl <danilovlaurent@pursuit.org>
2020-09-26 16:42:26 -0400

    add css drop down tab

--- frontend/src/features/posts/Posts.js
+++ frontend/src/features/posts/Posts.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./css/posts.css";
 import img from "../../images/user.png";
+import down from "../../images/download.png";
 import Hashtags from "../hashtags/Hashtags";
 
 const Posts = ({ posts = [], userName, name }) => {
@@ -15,57 +16,63 @@ const Posts = ({ posts = [], userName, name }) => {
                   <div className="postBody">
                     <div className="postBodySpace"></div>
                     <div className="postBodyCont">
-                      <div className="postAvy">
-                        <div className="postAvyCont">
-                          <img alt="" style={{ height: "45px" }} src={img} />
+                      <div className="postBodyItems">
+                        <div className="postAvy">
+                          <div className="postAvyCont">
+                            <img alt="" style={{ height: "45px" }} src={img} />
+                          </div>
                         </div>
-                      </div>
 
-                      <div className="postText">
-                        <div className="postTitle">
-                          <div className="postTitleCont">
-                            <div className="postTitleTain">
-                              <div className="postTitleItems">
-                                <div className="itemsHolder">
-                                  <span
-                                    style={{
-                                      color: "white",
-                                      fontWeight: "bolder",
-                                    }}
-                                  >
-                                    {name !== undefined ? name : post.name}
-                                  </span>
-                                  <span
-                                    style={{
-                                      flexShrink: 1,
-                                      marginLeft: "5px",
-                                    }}
-                                  >
-                                    @
-                                    {userName !== undefined
-                                      ? userName
-                                      : post.user_name}
-                                  </span>
+                        <div className="postText">
+                          <div className="postTitle">
+                            <div className="postTitleCont">
+                              <div className="postTitleTain">
+                                <div className="postTitleItems">
+                                  <div className="itemsHolder">
+                                    <span
+                                      style={{
+                                        color: "white",
+                                        fontWeight: "bolder",
+                                      }}
+                                    >
+                                      {name !== undefined ? name : post.name}
+                                    </span>
+                                    <span
+                                      style={{
+                                        flexShrink: 1,
+                                        marginLeft: "5px",
+                                      }}
+                                    >
+                                      @
+                                      {userName !== undefined
+                                        ? userName
+                                        : post.user_name}
+                                    </span>
+                                  </div>
                                 </div>
                               </div>
                             </div>
                           </div>
-                        </div>
-                        <div className="postTextCont">
-                          <div className="pTexts">
-                            <span
-                              style={{
-                                fontWeight: "400",
-                                fontSize: "15px",
-                                color: "rgb(255, 255, 255)",
-                              }}
-                            >
-                              {post.post_body}
-                            </span>
+                          <div className="postTextCont">
+                            <div className="pTexts">
+                              <span
+                                style={{
+                                  fontWeight: "400",
+                                  fontSize: "15px",
+                                  color: "rgb(255, 255, 255)",
+                                }}
+                              >
+                                {post.post_body}
+                              </span>
 
-                            <Hashtags hashtags={post.tags ? post.tags : []} />
+                              <Hashtags hashtags={post.tags ? post.tags : []} />
+                            </div>
                           </div>
                         </div>
+                      </div>
+
+                      <div className="postDropDownMenu">
+                        <img style={{ height: "2vh" }} alt="" src={down} />
                       </div>
                     </div>
                   </div>
